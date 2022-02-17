d77ec0a3c620ca0086dbccedfb163241f5601d64
2wnstjq2 <57422050+2wnstjq2@users.noreply.github.com>
2022-02-17 11:31:20 +0900

    retest
    restart cording

---
+++ pi1.py
@@ -0,0 +1,17 @@
+
+print('wow'+ '\n'+
+      '123') #message
+
+a=[11,1,'sk']
+b=[10]
+#print(a+b)
+'''
+for x in a:
+ print(x) #for / print must' '
+'''
+print(a[2])
+
+print(a*2)
+
+if a[0] != b[0]:
+    print('error')
